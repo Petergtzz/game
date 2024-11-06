d52dc4e5b8a2c9b2660bc460248179417e6596c8
Pedro <peter.gtz2020@gmail.com>
2024-11-05 18:10:44 -0600

    Pulled screen setup out of event handler

--- src/main.c
+++ src/main.c
@@ -22,12 +22,17 @@ typedef struct {
     u32 bytes_per_pixel; 
 } screen_buffer;
 
+typedef struct {
+    int height;
+    int width;
+} screen_dimensions;
+
 global_variable bool running;
 global_variable screen_buffer global_screen_buffer;
 
 SDL_Texture *color_texture;
 
-internal void osx_resize_screen(SDL_Renderer *renderer, 
+internal void osx_setup_screen(SDL_Renderer *renderer, 
                                 screen_buffer *buffer,
                                 int width,
                                 int height)
@@ -46,7 +51,8 @@ internal void osx_resize_screen(SDL_Renderer *renderer,
     buffer->memory = malloc(buffer->bytes_per_pixel * 
                             buffer->width * 
                             buffer->height);
-
+    
+    // Todo: Should this function call be here??
     color_texture = SDL_CreateTexture(renderer,
                                       SDL_PIXELFORMAT_ARGB8888,
                                       SDL_TEXTUREACCESS_STREAMING,
@@ -54,9 +60,7 @@ internal void osx_resize_screen(SDL_Renderer *renderer,
                                       buffer->height);
 }
 
-internal void osx_process_event(SDL_Window *window, 
-                                screen_buffer *buffer,
-                                SDL_Renderer *renderer)
+internal void osx_process_event(SDL_Window *window) 
 {
     SDL_Event event;
     SDL_PollEvent(&event);
@@ -65,6 +69,7 @@ internal void osx_process_event(SDL_Window *window,
     {
         case SDL_WINDOWEVENT:
         {
+            /*
             switch (event.window.event)
             {
                 case SDL_WINDOWEVENT_RESIZED:
@@ -74,6 +79,7 @@ internal void osx_process_event(SDL_Window *window,
                     osx_resize_screen(renderer, &global_screen_buffer, width, height);
                 } break; 
             } break;
+            */
         } break;
 
         case SDL_QUIT:
@@ -89,40 +95,45 @@ internal void osx_process_event(SDL_Window *window,
     }
 }
 
-internal void osx_draw_to_screen(SDL_Renderer *renderer, 
-                                 screen_buffer buffer, 
-                                 u32 x_offset, 
-                                 u32 y_offset)
+internal void osx_update_screen(screen_buffer *buffer, 
+                                u32 x_offset, 
+                                u32 y_offset)
 {
-    buffer.pitch = buffer.width * sizeof(u32);
-
-    u8 *row = (u8*)buffer.memory;
-    for (int y = 0; y < buffer.height; y++)
+    // Remember: big-endian architecture -- we specified pixel format as ARGB, 
+    // but in memory pixels are stored as BGRA
+    u8 *row = (u8*)buffer->memory;
+    for (int y = 0; y < buffer->height; y++)
     {
         u32 *pixel = (u32*)row;
-        for (int x = 0; x < buffer.width; x++)
+        for (int x = 0; x < buffer->width; x++)
         {
             u8 blue  = (x + x_offset);
-            u8 green = (y + y_offset);
+            u8 red   = (y + y_offset);
 
-            *pixel++ =((green << 8) | blue);
+            *pixel++ =((red << 16) | blue);
         }
 
-        row += buffer.pitch;
+        row += buffer->pitch;
     }
+}
 
+internal void osx_render_to_screen(SDL_Renderer *renderer, screen_buffer buffer)
+{
     SDL_UpdateTexture(color_texture, 
                       NULL, 
                       buffer.memory, 
-                      (u32)(buffer.width * sizeof(u32)));
-    SDL_RenderCopy(renderer, color_texture, NULL, NULL);
+                      buffer.pitch);
+    SDL_RenderCopy(renderer, 
+                   color_texture, 
+                   NULL, 
+                   NULL);
     SDL_RenderPresent(renderer);
 
 }
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0)
     {
         fprintf(stderr, "Error: Could not initialize SDL.\n");
     }
@@ -144,7 +155,8 @@ int main(int argc, char *argv[])
         if(renderer)
         {
             // Todo: Change function name
-            //osx_setup_screen(renderer, &global_screen_buffer);
+            osx_setup_screen(renderer, &global_screen_buffer, 
+                             display_area.w, display_area.h);
 
             u32 x_offset = 0;
             u32 y_offset = 0;
@@ -154,10 +166,18 @@ int main(int argc, char *argv[])
             while(running)
             {
                 // Process input
-                osx_process_event(window, &global_screen_buffer, renderer);
+                // TODO: Function does not take either global buffer or render.
+                // Make sure to clean it up. 
+                osx_process_event(window);
+                
+                SDL_GameController *game_controller;
+                SDL_GameControllerGetAttached(game_controller);
 
                 // Render
-                osx_draw_to_screen(renderer, global_screen_buffer, x_offset, y_offset);
+                osx_update_screen(&global_screen_buffer, x_offset, y_offset);
+                osx_render_to_screen(renderer, global_screen_buffer);
+
+                // Todo: Should I be clearing the memory buffer in each frame??
                 
                 x_offset++;
             }
